d83e8210b869fceb5a4bc5420ddc0204c0ace87e
Davies Fatimah Omowonuola <daviesfatimah97@gmail.com>
2022-03-04 23:35:10 -0800

    add user profile page style

--- deji/src/pages/singleUser/index.js
+++ deji/src/pages/singleUser/index.js
@@ -1,39 +1,47 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import Card from "../../components/Cards";
+import "./style.css";
 
 const SingleUser = () => {
   const location = useLocation();
 
   return (
-    <div>
-      <h1>User Profile</h1>
-      <div>
-        <h3>{location?.state?.name}</h3>
-        <p>{location?.state?.username}</p>
-        <p>{location?.state?.phone}</p>
-        <a href={`https://${location?.state?.website}`}>
-          {location?.state?.website}
-        </a>
+    <div className="single-user">
+      <div className="user-card">
+        <h1>User Profile</h1>
+        <Card>
+          <h3>Name: {location?.state?.name}</h3>
+          <p>Username: {location?.state?.username}</p>
+          <p>Phone: {location?.state?.phone}</p>
+          <a href={`https://${location?.state?.website}`}>
+            {location?.state?.website}
+          </a>
+        </Card>
       </div>
 
-      <h1>Address</h1>
-      <div>
-        <h3>{location?.state?.address?.city}</h3>
-        <p>{location?.state?.address?.street}</p>
-        <p>{location?.state?.address?.suite}</p>
-        <p>{location?.state?.address?.zipcode}</p>
-        <p>
-          {location?.state?.address?.geo?.lat},
-          {location?.state?.address?.geo?.lng}
-        </p>
+      <div className="user-card">
+        <h1>Address</h1>
+        <Card>
+          <h3>City: {location?.state?.address?.city}</h3>
+          <p>Street: {location?.state?.address?.street}</p>
+          <p>Suite: {location?.state?.address?.suite}</p>
+          <p>Zipcode: {location?.state?.address?.zipcode}</p>
+          <p>
+            GEO:
+            {location?.state?.address?.geo?.lat},
+            {location?.state?.address?.geo?.lng}
+          </p>
+        </Card>
       </div>
-
-      <h1>Company</h1>
-      <div>
-        <h3>{location?.state?.company?.bs}</h3>
-        <p>{location?.state?.company?.name}</p>
-        <p>{location?.state?.company?.catchPhrase}</p>
-        <p></p>
+      <div className="user-card">
+        <h1>Company</h1>
+        <Card>
+          <h3>BS: {location?.state?.company?.bs}</h3>
+          <p>Name: {location?.state?.company?.name}</p>
+          <p>CatchPhrase: {location?.state?.company?.catchPhrase}</p>
+          <p></p>
+        </Card>
       </div>
     </div>
   );
