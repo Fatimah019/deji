a1fa75529525b70fd26f42e9caade4a0f42a2053
DEJI ADEBAYO <adebayodeji14@yahoo.com>
2022-03-05 21:16:55 +0200

    remove unwanted file

--- .history/deji/src/store/index_20220305125949.js
+++
@@ -1,7 +0,0 @@
-import thunk from "redux-thunk";
-import rootReducer from "./rootReducer";
-import { createStore, applyMiddleware } from "redux";
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
